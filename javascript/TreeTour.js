3833ac73ff47a32679acb5facdd3173eee2799d7
Gould <gouldar02@JMACADMIN002.local>
2012-10-18 17:09:57 -0400

    Added (incomplete) marker highlight functionality

--- javascript/TreeTour.js
+++ javascript/TreeTour.js
@@ -1,7 +1,23 @@
 var Tree = Backbone.Model.extend({
-	initialize: function (domNode) {
-		this.set('domNode', $(domNode));
+	markerTemplate: _.template('<div class="map-marker" style="left:<%= x %>px; top:<%= y %>px"><%= index %></div>'),
+
+	initialize: function (domNode, index) {
+		var d = $(domNode);
+		this.set('domNode', d);
 		this.set('name', $('h2', domNode).text());
+
+		var coords = d.attr('data-map-coords');
+
+		if(coords) {
+			coords = coords.split(',');
+			var mapOffset = TreeTour.mapImage.offset();
+
+			this.set('marker', $(this.markerTemplate({
+				x: parseInt(coords[0], 10) + mapOffset.left,
+				y: parseInt(coords[1], 10) + mapOffset.top,
+				index: index
+			})).appendTo(TreeTour.body));
+		}
 	}
 });
 
@@ -9,14 +25,19 @@ var TreeTour = {
 	trees: [],
 	activeTreeMargin: 8,
 	activeTree: null,
+	body: null,
+	mapImage: null,
+	activeMarkerClass: 'active',
 
 	initialize: function () {
 		var self = this;
 		this.doc = $(document);
+		this.body = $('body');
 		this.details = $('#details');
+		this.mapImage = $('#map img');
 
 		$('.tree', this.details).each(function (idx, t) {
-			self.trees.push(new Tree(t));
+			self.trees.push(new Tree(t, idx + 1));
 		});
 
 		this.firstTreeOffset = this.trees[0].get('domNode').offset().top;
@@ -51,6 +72,8 @@ var TreeTour = {
 
 	activeTreeChanged: function (oldTree, newTree) {
 		console.log('tree changed to: "' + newTree.get('name') + '"');
+		//if(oldTree) oldTree.get('marker').removeClass(this.activeMarkerClass);
+		//if(newTree) newTree.get('marker').addClass(this.activeMarkerClass);
 	}
 };
 
